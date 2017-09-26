f4a09dff502b616a6eda2e68ab0903375311a234
Roman Voropaev <voropaev.roma@gmail.com>
2017-09-26 16:27:15 +0300

    Подсветка вопросов во фласке

--- taskB/webapp.py
+++ taskB/webapp.py
@@ -24,15 +24,46 @@ template = """
 @app.route("/")
 def taskB():
     i = randint(0, len(df))
-    text = highlight(df.paragraph[i],df.answer[i])
-    return render_template_string(template, i=i, paragraph=text, question=df.question[i], answer=df.answer[i])
+    answer = df.answer[i].lower().strip(' ').strip('.')
+    question = df.question[i].strip(' ').strip('?')
+    text = highlight(df.paragraph[i], question, answer)
+    return render_template_string(template, i=i, paragraph=text, question=df.question[i], answer=answer)
 
-def highlight(text, answer):
+def find_sent_end(text, pos):
+    for i in range(pos+1, len(text)):
+        if text[i-1:i+1] == '. ' and text[i+1].isupper():
+            return i
+    # reach end of text
+    return len(text)-1
+
+def find_sent_start(text, pos):
+    for i in range(pos-1, 0, -1):
+        if text[i-1:i+1] == '. ' and text[i+1].isupper():
+            return i
+    # reach start of text
+    return 0
+
+def highlight(text, question, answer):
     try:
-        start = text.index(answer)
+        answer_pos = text.lower().index(answer.lower())
     except ValueError:
         print("Answer not found in text!")
         return text
-    end = start + len(answer)
-    text = f"{text[:start]}<b>{text[start:end]}</b>{text[end:]}"
+    sent_start = find_sent_start(text, answer_pos)
+    sent_end = find_sent_end(text, answer_pos)
+    sent = text[sent_start:sent_end]
+    answer_pos_in_sent = answer_pos - sent_start
+    sent = f"{sent[:answer_pos_in_sent]}<b>{answer}</b>{sent[answer_pos_in_sent+len(answer):]}"
+    q_words = question.split(' ')
+    for w in q_words:
+        try:
+            i = sent.index(w)
+            j = i+len(w)
+            sent = f"{sent[:i]}<span style='color:red'>{w}</span>{sent[j:]}"
+        except ValueError:
+            pass
+    text = f"{text[:sent_start]}<span style='background-color:yellow'>{sent}</span>{text[sent_end:]}"
     return text
+
+if __name__=='__main__':
+    app.run(port=5000, debug=True)
