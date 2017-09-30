e8eece830bf2eb661c6021055207c122c8874442
Roman Voropaev <voropaev.roma@gmail.com>
2017-09-30 04:12:10 +0300

    Рефакторинг вебприложения для B

--- taskB/webapp.py
+++ taskB/webapp.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template_string
 app = Flask(__name__)
 
+import nltk
 import pandas as pd
 df = pd.read_csv('../train_task_b.csv')
 from random import randint
+from pymorphy2.analyzer import MorphAnalyzer
+morph = MorphAnalyzer()
 
 template = """
 <html>
@@ -29,40 +32,28 @@ def taskB():
     text = highlight(df.paragraph[i], question, answer)
     return render_template_string(template, i=i, paragraph=text, question=df.question[i], answer=answer)
 
-def find_sent_end(text, pos):
-    for i in range(pos+1, len(text)):
-        if text[i-1:i+1] == '. ' and text[i+1].isupper():
-            return i
-    # reach end of text
-    return len(text)-1
-
-def find_sent_start(text, pos):
-    for i in range(pos-1, 0, -1):
-        if text[i-1:i+1] == '. ' and text[i+1].isupper():
-            return i
-    # reach start of text
-    return 0
+def wrap_tag(text, word, open_tag, close_tag):
+    return text.replace(word, '{1}{0}{2}'.format(word, open_tag, close_tag))
 
 def highlight(text, question, answer):
     try:
-        answer_pos = text.lower().index(answer.lower())
-    except ValueError:
-        print("Answer not found in text!")
+        answer = answer.lower()
+        sentences = nltk.sent_tokenize(text)
+        sent = next(filter(lambda s: answer in s.lower(), sentences))
+    except StopIteration:
+        print("Answer not found in text! This means error in sentence tokenizer")
         return text
-    sent_start = find_sent_start(text, answer_pos)
-    sent_end = find_sent_end(text, answer_pos)
-    sent = text[sent_start:sent_end]
-    answer_pos_in_sent = answer_pos - sent_start
-    sent = f"{sent[:answer_pos_in_sent]}<b>{answer}</b>{sent[answer_pos_in_sent+len(answer):]}"
-    q_words = question.split(' ')
+
+    orig = sent
+    sent = sent.lower()
+    # TODO:here morphy
+    sent = wrap_tag(sent, answer, '<b>', '</b>')
+    q_words = nltk.wordpunct_tokenize(question)
     for w in q_words:
-        try:
-            i = sent.index(w)
-            j = i+len(w)
-            sent = f"{sent[:i]}<span style='color:red'>{w}</span>{sent[j:]}"
-        except ValueError:
-            pass
-    text = f"{text[:sent_start]}<span style='background-color:yellow'>{sent}</span>{text[sent_end:]}"
+        if len(w)<3: continue
+        sent = wrap_tag(sent, w, '<span style=\'color:red\'>', '</span>')
+    sent = '<span style=\'background-color:yellow\'>{}</span>'.format(sent)
+    text = text.replace(orig, sent)
     return text
 
 if __name__=='__main__':
