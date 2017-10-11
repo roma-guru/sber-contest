1fed95be70436bb103e27988fbd7b195c1520bd2
Artiomio <peysahovsky@gmail.com>
2017-10-11 06:18:58 +0300

    Python wrapper for malt parser + vis.js graph

---
+++ taskB/maltparse_file.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -jar maltparser-1.9.1.jar -c russian_syntags -i $1 -m parse
--- taskB/txt2conllu.py
+++ taskB/txt2conllu.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import nltk
 import sys
 from pymorphy2 import MorphAnalyzer
@@ -7,21 +8,27 @@ m = MorphAnalyzer()
 oc2ud = converters.converter('opencorpora-int','ud20')
 
 
+def text_to_conllu(txt):
+    sents = nltk.sent_tokenize(txt)
+    out_text = []
+    for i,s in enumerate(sents):
+        s = s.replace('\n','')
+        tokens = nltk.word_tokenize(s)
+        out_text.append("\n# sent: {}".format(i))
+        out_text.append("# text: {}".format(s))
+        for j,t in enumerate(tokens):
+            t = m.parse(t)[0]
+            pos, feats = oc2ud(str(t.tag)).split(' ')
+            out_text.append("{0}\t{1}\t{2}\t{3}\t_\t{4}\t_\t_\t_\t_".format(j+1, t.word, 
+                                                       t.normal_form, pos,
+                                                        feats))
+    return "\n".join(out_text)[1:]
+
+
+
+
 if __name__=='__main__':
     infile = sys.argv[1]
     with open(infile) as f:
         txt = f.read()
-
-    sents = nltk.sent_tokenize(txt)
-    for i,s in enumerate(sents):
-        s = s.replace('\n','')
-        tokens = nltk.word_tokenize(s)
-        print("# sent: {}".format(i))
-        print("# text: {}".format(s))
-        for j,t in enumerate(tokens):
-            t = m.parse(t)[0]
-            pos, feats = oc2ud(str(t.tag)).split(' ')
-            print("{0}\t{1}\t{2}\t{3}\t_\t{4}\t_\t_\t_\t_".format(j+1, t.word, 
-                                                       t.normal_form, pos,
-                                                        feats))
-        print()
+        print(text_to_conllu(txt))
