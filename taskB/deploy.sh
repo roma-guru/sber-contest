bb058e2baa3282a1a16d6e8f5b78e1498337c2ad
Roman Voropaev <voropaev.roma@gmail.com>
2017-10-19 11:16:09 +0300

    Fix in deploy script

--- taskB/deploy.sh
+++ taskB/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rsync -vz {webapp.py,russian_syntags.mco,maltparser-1.9.1.jar,lib,template.html} \
+rsync -avz {webapp.py,russian_syntags.mco,maltparser-1.9.1.jar,lib,template.html} \
     neurolab:webapp/
